2ee085cabcaf0bdaf35755039a2cbf54db5dbc81
Popon <joaquimmy@yahoo.com>
2019-05-24 10:05:50 -0300

    Added a remove method and additional checks

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-class Events {
-  constructor() {
-    this.events = new Map();
-  }
+function Events(config) {
+  this.events = new Map();
+  this.suppressMissingHandlerError = config && config.suppressMissingHandlerError;
 
-  on(eventName, handler) {
+  this.on = function(eventName, handler) {
     if (this.events.has(eventName)) {
       this.events.set(eventName, [...this.events.get(eventName), handler]);
     } else {
       this.events.set(eventName, [handler]);
     }
-  }
+  };
 
-  remove(eventName, handler) {
+  this.remove = function(eventName, handler) {
+    if (!handler) {
+      throw new Error('Specify a event name and a event handler to remove.');
+    }
     if (this.events.has(eventName)) {
       const filteredHandlers = this.events
         .get(eventName)
         .filter(h => h !== handler);
-      this.events.set(eventName, filteredHandlers);
+      if (filteredHandlers.length <= 0) {
+        this.events.delete(eventName);
+      } else {
+        this.events.set(eventName, [...filteredHandlers]);
+      }
     }
-  }
+  };
 
-  send(eventName, data) {
+  this.send = function(eventName, data) {
     if (!this.events.has(eventName)) {
-      throw new Error(`Event ${eventName} not found.`);
+      if (this.suppressMissingHandlerError) {
+        return;
+      } else {
+        throw new Error(`Event ${eventName} not found.`);
+      }
     }
-    try {
-      this.events.get(eventName).forEach(evt => evt(data));
-    } catch (e) {
-      console.error(e);
-    }
-  }
+    this.events.get(eventName).forEach(handler => handler(data));
+  };
 }
 
 module.exports = Events;
