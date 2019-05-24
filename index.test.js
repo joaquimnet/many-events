b4847c89f3cd4474d84db5d7c1572241b57f45bc
Popon <joaquimmy@yahoo.com>
2019-05-24 10:05:01 -0300

    Added tests

---
+++ index.test.js
@@ -0,0 +1,45 @@
+const ManyEvents = require('./index');
+
+test('can send and handle events', done => {
+  function myPeanutButter() {
+    const events = new ManyEvents();
+    events.on('test', data => {
+      expect(data).toBe('peanut butter');
+      done();
+    });
+    events.send('test', 'peanut butter');
+  }
+  myPeanutButter();
+});
+
+test('should throw missing handler error', () => {
+  function thisWillThrow() {
+    const events = new ManyEvents();
+    events.send('anything', { any: 'thing' });
+  }
+  expect(thisWillThrow).toThrow('Event anything not found.');
+});
+
+test('should throw missing handler error after removing handler', () => {
+  let count = 0;
+  function increaseCount() {
+    count++;
+  }
+  function countAndThrow() {
+    const events = new ManyEvents();
+    events.on('increase-count', increaseCount);
+    events.send('increase-count', null);
+    events.remove('increase-count', increaseCount);
+    events.send('increase-count', null);
+  }
+  expect(countAndThrow).toThrow('Event increase-count not found.');
+  expect(count).toEqual(1);
+});
+
+test('should NOT throw missing handler error', () => {
+  function thisWillThrow() {
+    const events = new ManyEvents({ suppressMissingHandlerError: true });
+    events.send('anything', { any: 'thing' });
+  }
+  expect(thisWillThrow).not.toThrow();
+});
